74e41b28b99a972cadee4876bfb334954a3c06a9
gp2593 <gaopeng.pb@gmail.com>
2014-03-12 02:31:25 +0000

    add new full-with-no-comment page template

---
+++ page-templates/full-width-no-comment.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template Name: Full-width Page Template, No Sidebar, No comment
+ */
+get_header(); ?>
+
+<div id="content">
+
+	<?php while( have_posts() ) : the_post(); ?>
+		<?php get_template_part( 'content', get_post_format() ); ?>
+			
+		<?php comments_template( '', true ); ?>
+	<?php endwhile; ?>
+
+</div> <!-- #content -->
+
+
+<div class="dummy"></div>
+<?php
+get_footer(); ?>
